c728a7abd1c905b147df20745f57f71038d87a74
ranjani-tech <hola2ranjani@gmail.com>
2022-04-04 11:40:52 +0100

    add new step in stepdefinitionss

--- src/test/java/com/globaltechchallenge/automationpracticedemo/web/StepDefinitions.java
+++ src/test/java/com/globaltechchallenge/automationpracticedemo/web/StepDefinitions.java
@@ -8,4 +8,14 @@ import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
+    @Given("User is on my store product home page")
+    public void user_is_on_my_store_product_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("user verify the page title {string}")
+    public void user_verify_the_page_title(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
 }
--- src/test/resources/com/globaltechchallenge/automationpracticedemo/web/automationpractice.feature
+++ src/test/resources/com/globaltechchallenge/automationpracticedemo/web/automationpractice.feature
@@ -3,3 +3,9 @@
 
 Feature: User should be able add product to cart
   and verify the total cost of the all product in the Cart
+
+
+  @AddAndRemoveProduct
+  Scenario: end to end journey till product checkout page.
+    Given User is on my store product home page
+    When user verify the page title "My Store"

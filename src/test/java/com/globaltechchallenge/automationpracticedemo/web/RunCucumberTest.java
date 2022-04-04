188a4d89ad808d923f26ae2e37032bf2aaea1c7b
ranjani-tech <hola2ranjani@gmail.com>
2022-04-04 11:42:43 +0100

    add cucumber configurations in stepdefinitions

--- src/test/java/com/globaltechchallenge/automationpracticedemo/web/RunCucumberTest.java
+++ src/test/java/com/globaltechchallenge/automationpracticedemo/web/RunCucumberTest.java
@@ -1,15 +1,28 @@
 package com.globaltechchallenge.automationpracticedemo.web;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
+import static io.cucumber.core.options.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/globaltechchallenge/automationpracticedemo/web")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+
+@ConfigurationParameters({
+        @ConfigurationParameter(
+                key =        PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME,
+                value="false"
+        ),
+        @ConfigurationParameter(
+                key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME,
+                value="true"
+        ),
+        @ConfigurationParameter(
+                key = PLUGIN_PROPERTY_NAME,
+                value ="pretty, html:cucumber-reports/cucumber-reports.html"
+        )
+})
 public class RunCucumberTest {
 }

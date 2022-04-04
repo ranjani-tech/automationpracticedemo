5fe2dadf07aef936a95acab09d9075ef77cd25a9
ranjani-tech <hola2ranjani@gmail.com>
2022-04-04 11:43:14 +0100

    add cucumber configurations in stepdefinitions

--- src/test/java/com/globaltechchallenge/automationpracticedemo/web/RunCucumberTest.java
+++ src/test/java/com/globaltechchallenge/automationpracticedemo/web/RunCucumberTest.java
@@ -1,6 +1,12 @@
 package com.globaltechchallenge.automationpracticedemo.web;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.junit.platform.suite.api.*;
+
+import java.io.IOException;
 
 import static io.cucumber.core.options.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -25,4 +31,30 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_P
         )
 })
 public class RunCucumberTest {
+        private DriverProvider driverProvider;
+        private PropertiesProvider propertiesProvider;
+
+        @Before
+        public void before(Scenario scenario) throws IOException {
+                propertiesProvider = new PropertiesProvider();
+                propertiesProvider.loadProperties();
+
+                driverProvider = new DriverProvider();
+                driverProvider.initDriver();
+        }
+
+
+        @AfterStep
+        public void takeScreeshot(Scenario scenario){
+                final byte[] screenshot = ((TakesScreenshot) driverProvider.getDriver())
+                        .getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png",  scenario.getName()); //stick it in the report
+                //driverProvider.getDriver().close();
+        }
+
+
+        @After
+        public void after(Scenario scenario) {
+                driverProvider.getDriver().quit();
+        }
 }
